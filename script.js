2b17679d18cb16fbdf10f736c1d2a152c3ff998f
Elijha Couch <31853975+ElijhaC@users.noreply.github.com>
2022-02-11 21:08:12 -0500

    My first Commit

---
+++ script.js
@@ -0,0 +1,46 @@
+
+
+
+/*----------------------toggle navbar -------------------*/
+const navToggler = document.querySelector(".nav-toggler");
+navToggler.addEventListener("click", toggleNav);
+
+function toggleNav(){
+    navToggler.classList.toggle("active");
+    document.querySelector(".nav").classList.toggle("open");
+}
+
+/* close nav when clicking ona nav iter*/
+document.addEventListener("click", function(e){
+    if(e.targer.closest(".nav-item")){
+        toggleNav();
+
+    }
+});
+
+/*-----------------------sticky header ----------------------*/
+window.addEventListener("scroll", function(){
+    if(this.pageYOffset > 60){
+        document.querySelector(".header").classList.add("sticky");
+    }
+    else{
+        document.querySelector(".header").classList.add("sticky");
+    }
+});
+
+/*----------------------Thank you pop up ---------------------*/
+$(document).on("scroll", function() {
+    var pageTop = $(document).scrollTop();
+    var pageBottom = pageTop + $(window).height();
+    var tags = $(".tag");
+  
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i];
+  
+      if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("visible");
+      } else {
+        $(tag).removeClass("visible");
+      }
+    }
+  });
